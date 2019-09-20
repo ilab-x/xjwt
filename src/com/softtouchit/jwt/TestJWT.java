ab90bd7e8c89e520ccb49902f64a4bb8843a822c
ilab-x <51390145+ilab-x@users.noreply.github.com>
2019-09-20 10:39:27 +0800

    Update TestJWT.java

--- src/com/softtouchit/jwt/TestJWT.java
+++ src/com/softtouchit/jwt/TestJWT.java
@@ -13,32 +13,32 @@ import net.sf.json.JSONObject;
 
 public class TestJWT {
 
-	public static void main(String[] args)
-	{
+    public static void main(String[] args)
+    {
 		try
 		{
-			String data= "";
-			// =================示例：解密xjwt (token也是一个xjwt);
-			String xjwt = "AAABbUy0XxsCAAAAAAABiDA%3D.xPwVH6y5s7tALHu1W3z4zX9Moo5j3qHhHylUxL2lVFzRKDBzQpK1YmrohX2gKKVE.zxDXPoreJXv8N1BAtMUcceupBM8nf0UcWQx5j0u6Ao0%3D";
-			data = dencrty(xjwt);
-			System.out.println(data);
-			
-			// =================示例：生成xjwt
-			
-			JSONObject param=new JSONObject();
-			param.put("username","test");
-			param.put("issuerId",KEY.issueId.toString());
-			String json=param.toString();
-			
-			data = encrty(json);
-			System.out.println(data);
-			
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-		
+		String data= "";
+		// =================示例：解密xjwt (token也是一个xjwt);
+		String xjwt = "AAABbUy0XxsCAAAAAAABiDA%3D.xPwVH6y5s7tALHu1W3z4zX9Moo5j3qHhHylUxL2lVFzRKDBzQpK1YmrohX2gKKVE.zxDXPoreJXv8N1BAtMUcceupBM8nf0UcWQx5j0u6Ao0%3D";
+		data = dencrty(xjwt);
+		System.out.println(data);
+
+		// =================示例：生成xjwt
+
+		JSONObject param=new JSONObject();
+		param.put("username","test");
+		param.put("issuerId",KEY.issueId.toString());
+		String json=param.toString();
+
+		data = encrty(json);
+		System.out.println(data);
+
+	} catch (Exception e)
+	{
+		e.printStackTrace();
 	}
+
+    }
     public static String dencrty(String xjwt) throws Exception {
     	//获取当前时间
     	long now = new Date().getTime();
@@ -52,12 +52,12 @@ public class TestJWT {
     }
     
     public static String encrty(String json) throws Exception {
-		//获取当前时间
-		long now=System.currentTimeMillis();
-		//创建JWT实例
-		JWT jwt=new JWT(KEY.secret,KEY.aeskey,now,KEY.issueId);
-		//创建payload用来装数据
-		ByteBuffer payload = ByteBuffer.allocate(1024).order(ByteOrder.BIG_ENDIAN);
+	//获取当前时间
+	long now=System.currentTimeMillis();
+	//创建JWT实例
+	JWT jwt=new JWT(KEY.secret,KEY.aeskey,now,KEY.issueId);
+	//创建payload用来装数据
+	ByteBuffer payload = ByteBuffer.allocate(1024).order(ByteOrder.BIG_ENDIAN);
         payload.put(json.getBytes("UTF-8")).flip();
 		//创建out对象
         ByteBuffer out = ByteBuffer.allocate(1024);
