76d7123fe7f1baedf56eb92c612eb1bdac93dc2f
ilab-x <51390145+ilab-x@users.noreply.github.com>
2019-10-09 14:26:21 +0800

    Update KEY.java

--- src/com/softtouchit/jwt/KEY.java
+++ src/com/softtouchit/jwt/KEY.java
@@ -1,8 +1,10 @@
 package jwt;
 
 public class KEY {
-		// 秘钥组：一个项目对应一个秘钥组。以下秘钥组为范例，请用拿到的秘钥组替换：
-  		public static String secret = "16ilab";
-  		public static String aeskey = "ILABymvfZ8UjEmShxRAB0b1Dtaa0uGjDOOJa/f0Mbuo4=";
-  		public static Long issueId=999999L;
+	// 秘钥组：一个项目对应一个秘钥组。以下秘钥组为范例，请用拿到的秘钥组替换：
+	public static String secret = "16ilab";
+	public static String aeskey = "ILABymvfZ8UjEmShxRAB0b1Dtaa0uGjDOOJa/f0Mbuo4=";
+	public static Long issueId = 999999L;
 }
+
+
